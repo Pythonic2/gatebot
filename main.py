ad4a5cabc32a6354e551213276856ee7a7c95f82
Pythonic2 <89715071+Pythonic2@users.noreply.github.com>
2025-01-03 18:50:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from time import time, sleep
+from time import time
 
 
 # Configurar o WebDriver (apenas uma vez)
@@ -88,6 +88,4 @@ def main():
 
 
 if __name__ == "__main__":
-    while True:
-        main()
-        sleep(1800)
+    main()
